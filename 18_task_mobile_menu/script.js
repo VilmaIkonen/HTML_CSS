fd193c3f792adeead2d71a8310f4568c9e32d20c
Tietohallinto <tietohallinto@BC-minimac030.local>
2020-10-07 13:45:23 +0300

    18_task added

---
+++ 18_task_mobile_menu/script.js
@@ -0,0 +1,7 @@
+const mobileMenu = () => {
+  const menu = document.querySelector("ul");
+  menu.classList.toggle("mobileMenu");
+};
+/*    OR     */
+
+// const mobileMenu = () => document.querySelector("ul").classList.toggle("mobileMenu");
